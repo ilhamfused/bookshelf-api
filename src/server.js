62818a8325555505e40668d630d401cf84798777
ilhamfused <irharijanto20@gmail.com>
2024-04-19 11:16:19 +0700

    done

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const Hapi = require("@hapi/hapi");
-// const routes = require("./routes");
+const routes = require("./routes");
 
 const init = async () => {
   const server = Hapi.server({
@@ -12,7 +12,7 @@ const init = async () => {
     },
   });
 
-  // server.route(routes);
+  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
